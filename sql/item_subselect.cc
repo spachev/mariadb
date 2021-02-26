c997e406fa624a0fcdc96be1ff7ab7fbd5e1b3ee
Varun Gupta <varun.gupta@mariadb.com>
2021-02-26 20:38:20 +0530

    MDEV-24519: Server crashes in Charset::set_charset upon SELECT
    The query causing the issue here has implicit grouping for we
    have to produce one row with special values for the aggregates
    (depending on each aggregate function), and NULL values for all
    non-aggregate fields.
    
    The subselect item where implicit grouping was being done, null_value
    for the subselect item was not being set for the case when the implicit
    grouping produces NULL values for the items in the select
    list of the subquery. This which was leading to the crash.
    
    The fix would be to set the null_value when all the values for the row
    column have NULL values.

--- sql/item_subselect.cc
+++ sql/item_subselect.cc
@@ -1319,7 +1319,17 @@ bool Item_singlerow_subselect::null_inside()
 void Item_singlerow_subselect::bring_value()
 {
   if (!exec() && assigned())
-    null_value= 0;
+  {
+    null_value= true;
+    for (uint i= 0; i < max_columns ; i++)
+    {
+      if (!row[i]->null_value)
+      {
+        null_value= false;
+        return;
+      }
+    }
+  }
   else
     reset();
 }
