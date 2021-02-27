ec6d92363444066ca8e6c9c52caab122ada56812
Varun Gupta <varun.gupta@mariadb.com>
2021-02-27 12:15:39 +0530

    MDEV-24519: Server crashes in Charset::set_charset upon SELECT
    Introduced a parameter null_value_inside to Item_cache that
    would store be set to TRUE if any of the arguments of the
    Item_cache are null.

--- sql/item_subselect.cc
+++ sql/item_subselect.cc
@@ -851,7 +851,7 @@ bool Item_subselect::expr_cache_is_needed(THD *thd)
 
 inline bool Item_in_subselect::left_expr_has_null()
 {
-  return (*(optimizer->get_cache()))->null_value;
+  return (*(optimizer->get_cache()))->null_value_inside;
 }
 
 
